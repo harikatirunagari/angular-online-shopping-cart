e43b7bca0df1f858144c47b29737b5bb01b8d2d5
Harika Tirunagari <harikasatagopam@gmail.com>
2019-06-08 23:58:30 +0530

    Create environment.ts

---
+++ app/environment/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production : false,
+  firebase:{
+    apiKey: "AIzaSyC4fAX91PNcddVz5YB_yj57UbK6_AUtlRs",
+    authDomain: "sample-1489.firebaseapp.com",
+    databaseURL: "https://sample-1489.firebaseio.com",
+    projectId: "sample-1489",
+    storageBucket: "sample-1489.appspot.com",
+    messagingSenderId: "631445424424"
+  }
+
+}
